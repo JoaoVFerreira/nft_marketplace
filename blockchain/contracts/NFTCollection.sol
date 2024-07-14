8bdc1c8b0875cb7af6f655aee22fef5ce87e1e2c
João Ferreira <joaovtf39@gmail.com>
2024-07-14 16:21:34 -0300

    feat: add NFTCollection.sol contract

---
+++ blockchain/contracts/NFTCollection.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTCollection is ERC721URIStorage {
+  	uint private _tokenIds;
+	address contractAddress; //marketplace
+	address owner;
+
+	constructor(address marketplaceAddress) ERC721("OpenC", "OPC") {
+		contractAddress = marketplaceAddress;
+		owner = msg.sender;
+	}
+
+	function mint(string memory uri) public returns (uint) {
+		uint tokenId = _tokenIds++;
+		_mint(msg.sender, tokenId);
+		_setTokenURI(tokenId, uri);
+		setApprovalForAll(contractAddress, true);
+
+		return tokenId;
+	}
+
+	function setApprovalForAll(address operator, bool approved) public virtual override (ERC721, IERC721) {
+		require(_msgSender() == owner || operator != contractAddress || approved, "Cannot remove marketplace approval");
+		_setApprovalForAll(_msgSender(), operator, approved);
+	}
+}
