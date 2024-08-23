97270953e60ecd25b2106b98502b48ec1a59c5e2
João Ferreira <joaovtf39@gmail.com>
2024-08-22 22:45:45 -0300

    feat: add main and create page

---
+++ dapp/src/app/create/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+function Create() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}
+
+export default Create;
