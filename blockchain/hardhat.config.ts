af9f9ec96d1f7c6797b575806403b675007230c1
João Ferreira <joaovtf39@gmail.com>
2024-08-26 21:51:33 -0300

    chore: downgrade ethers

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,23 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-  networks: {
-    polygonTestNet: {
-      url: process.env.RPC_URL,
-      chainId: parseInt(`${process.env.CHAIN_ID}`),
-      accounts: {
-        mnemonic: process.env.SECRET
-      }
-    }
-  },
-  etherscan: {
-    apiKey: process.env.API_KEY
-  }
+	solidity: "0.8.24",
+	networks: {
+		polygonTestNet: {
+			url: process.env.RPC_URL,
+			chainId: Number.parseInt(`${process.env.CHAIN_ID}`),
+			accounts: {
+				mnemonic: process.env.SECRET,
+			},
+		},
+		bsctest: {
+			url: process.env.BSC_URL,
+			chainId: Number.parseInt(`${process.env.CHAIN_ID_BSC}`),
+			accounts: {
+				mnemonic: process.env.SECRET,
+			},
+		},
+	},
+	etherscan: {
+		apiKey: process.env.API_KEY,
+	},
 };
 
 export default config;
